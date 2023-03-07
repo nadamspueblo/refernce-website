1f53e4ecb5802a00b562eb57e5df3a3fec494f9f
Nathan Adams <47441281+nadamspueblo@users.noreply.github.com>
2023-03-07 17:38:16 +0000

    Completed 7.1

---
+++ 7.1-arrays-intro.js
@@ -0,0 +1,19 @@
+function example() {
+  const topStudents = ["Christian", "Kristian", "Shakka", "Jaime", "AJ"];
+  var name = topStudents[2];
+  console.log(name);
+  topStudents[0] = "Mauricio";
+  console.log(topStudents);
+  topStudents = "Mauricio";
+  console.log(topStudents);
+  console.log(topStudents[0]);
+}
+
+function problem1() {
+
+}
+
+
+function problem2(){
+
+}
